005dbf99da529c9229db2bf55331d20470729824
Saksham Gupta <sakshamgupta.ai@gmail.com>
2024-03-09 15:29:13 +0530

    Khichdi

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement :{}", ann);
+
     if x.len() > y.len() {
         x
     } else {
@@ -7,11 +14,9 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
-    let string1 = String::from("long string is long");
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    {
-        let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        println!("The longest string is {}", result);
-    }
+    let result = longest_with_an_announcement(string1.as_str(), string2, "Today is birthday");
+    println!("The longest string is {}", result);
 }
